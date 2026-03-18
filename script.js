6933bbd4ec423987c3a6de2be3ec9b200525b132
TheTringLink <138830870+TheTringLink@users.noreply.github.com>
2026-03-18 08:50:02 +0800

    Update script.js

--- script.js
+++ script.js
@@ -59,10 +59,10 @@ document.addEventListener('DOMContentLoaded', function() {
             if (video) {
                 if (video.paused) {
                     video.play();
-                    toggleIcon.innerText = '∷';
+                    toggleIcon.innerHTML = '<i class="fa fa-pause" aria-hidden="true"></i>';
                 } else {
                     video.pause();
-                    toggleIcon.innerText = '◇';
+                    toggleIcon.innerHTML = '<i class="fa fa-play" aria-hidden="true"></i>';
                 }
             }
 
